3fef74e4fced39d4ad69e27d98f97ec51eed0bd8
Noah Ryan <nsmryan@gmail.com>
2020-08-08 14:23:21 -0400

    Primary header seems to work

--- src/main.zig
+++ src/main.zig
@@ -4,25 +4,6 @@ const std = @import("std");
 const testing = std.testing;
 const assert = std.debug.assert;
 
-pub fn EndianWrapped(comptime typ: type, comptime endian: Endian) type {
-    return packed struct {
-        val: typ,
-
-        const Self = @This();
-
-        pub fn new(val: typ) Self {
-            return Self{ .val = val };
-        }
-
-        pub fn swap(self: Self) EndianWrapped(type, opposite_endian(endian)) {
-            const swapped_val = generic_swap(typ, self.val);
-
-            const swapped = EndianWrapped(type, opposite_endian(endian)).new(swapped);
-            return swapped;
-        }
-    };
-}
-
 pub const Apid = u11;
 
 pub const SecHeaderPresent = enum(u1) {
@@ -43,48 +24,6 @@ pub const SeqFlag = enum(u2) {
     LastSegment = 2,
     Unsegmented = 3,
 };
-
-pub const CcsdsPrimaryRaw = packed struct {
-    control: u16,
-    sequence: u16,
-    length: u16,
-};
-
-pub fn opposite_endian(endian: Endian) Endian {
-    switch (endian) {
-        Endian.Big => {
-            return Endian.Little;
-        },
-
-        Endian.Little => {
-            return Endian.Big;
-        },
-    }
-}
-
-pub fn generic_swap(comptime T: type, val: T) T {
-    comptime const num_bits = @bitSizeOf(T);
-
-    if (num_bits == 0) {
-        @panic("0 bit size may indicate comptime type. This function does not make sense at comptime");
-    }
-
-    if (num_bits % 8 != 0) {
-        @panic("Swapping a type that is not bit-aligned does not make sense!");
-    }
-
-    comptime const num_bytes = num_bits / 8;
-
-    // NOTE consider using inline
-    var bytes = @bitCast([num_bytes]u8, val);
-    const mid_byte_index = num_bytes / 2;
-    var index = 0;
-    while (index < mid_byte_index) {
-        const tmp = bytes[index];
-        bytes[index] = bytes[num_bytes - index];
-        bytes[num_bytes - index] = tmp;
-    }
-}
 
 pub const CcsdsControl = packed struct {
     apid: Apid,
@@ -123,22 +62,28 @@ pub const CcsdsPrimary = packed struct {
 
     pub fn new(apid: Apid, packet_type: PacketType) Self {
         var pri = Self{
-            .control = CcsdsControl.new(apid, packet_type),
+            .control = CcsdsControl.new(0, PacketType.Data),
             .sequence = CcsdsSequence.new(),
             .length = CcsdsLength.new(),
         };
 
+        pri.set_apid(apid);
+
+        // TODO set packet_type
+
         return pri;
     }
 
+    // NOTE consider a function that takes the type, casts, swaps, and casts back.
+    // NOTE byteSwap apparently works with vectors, so perhaps can cast any object to bytes and still swap
     pub fn set_apid(self: *Self, apid: Apid) void {
-        var swapped = generic_swap(CcsdsControl, self.control);
+        var swapped = @bitCast(CcsdsControl, @byteSwap(u16, @bitCast(u16, self.control)));
         swapped.apid = apid;
-        self.control = generic_swap(CcsdsControl, swapped);
+        self.control = @bitCast(CcsdsControl, @byteSwap(u16, @bitCast(u16, swapped)));
     }
 
     pub fn get_apid(self: Self) Apid {
-        const swapped = generic_swap(self.control);
+        const swapped = @bitCast(CcsdsControl, @byteSwap(u16, @bitCast(u16, self.control)));
         return swapped.apid;
     }
 };
@@ -148,5 +93,5 @@ test "primary header" {
     const apid_raw = 0x1200;
     const pri = CcsdsPrimary.new(apid, PacketType.Data);
 
-    assert(pri.get_apid() == 0x0012);
+    assert(pri.get_apid() == apid);
 }
